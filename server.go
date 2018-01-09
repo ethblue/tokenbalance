d525afb90c73e1246ced632912afb7fcf3bccb78
Alexander <alexanderattar@gmail.com>
2018-01-09 17:45:38 -0500

    Initial testing of fetching all tokens listed in an array

--- server.go
+++ server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-)
+	"os"
 
+	"github.com/gorilla/mux"
+)
 
 type BalanceResponse struct {
 	Name       string `json:"name,omitempty"`
@@ -16,6 +19,13 @@ type BalanceResponse struct {
 	EthBalance string `json:"eth_balance,omitempty"`
 	Decimals   uint8  `json:"decimals,omitempty"`
 	Block      uint64 `json:"block,omitempty"`
+}
+
+type T struct {
+	Address string `json:"address"`
+	Symbol  string `json:"symbol"`
+	Decimal int    `json:"decimal"`
+	Type    string `json:"type"`
 }
 
 type ErrorResponse struct {
@@ -61,6 +71,55 @@ func getInfoHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getAllTokensHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	wallet := vars["wallet"]
+	var output []BalanceResponse
+
+	raw, err := ioutil.ReadFile("./ethTokens.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var tokens []T
+	json.Unmarshal(raw, &tokens)
+
+	for _, t := range tokens {
+		log.Println("Fetching Wallet:", wallet, "at Contract:", t.Address)
+		name, balance, token, decimals, ethAmount, block, err := GetAccount(t.Address, wallet)
+
+		if err != nil {
+			m := ErrorResponse{
+				Error:   true,
+				Message: "could not find contract address",
+			}
+			msg, _ := json.Marshal(m)
+			w.Write(msg)
+			return
+		}
+
+		new := BalanceResponse{
+			Name:       name,
+			Symbol:     token,
+			Decimals:   decimals,
+			Wallet:     wallet,
+			Balance:    balance,
+			EthBalance: ethAmount,
+			Block:      block,
+		}
+
+		output = append(output, new)
+	}
+
+	j, err := json.Marshal(output)
+
+	if err == nil {
+		w.Write(j)
+	}
+}
+
 func getTokenHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 
@@ -86,10 +145,10 @@ func StartServer() {
 	http.ListenAndServe(UseIP+":"+UsePort, nil)
 }
 
-
 func Router() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/balance/{contract}/{wallet}", getTokenHandler).Methods("GET")
 	r.HandleFunc("/token/{contract}/{wallet}", getInfoHandler).Methods("GET")
+	r.HandleFunc("/tokens/{wallet}", getAllTokensHandler).Methods("GET")
 	return r
 }
